06a343e36759f358dec8dd62bbbb1fd8004d7d58
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-11-25 07:12:11 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -6,8 +6,8 @@ from flask_login import LoginManager
 application = Flask(__name__)
 application.config['SECRET_KEY'] = '1dfc4dedcd5b2ffa3a090dfc34f845fd'
 
- application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
- application.config['SQLALCHEMY_BINDS'] ={'gd_course': 'sqlite:///gd_course.db',
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'gd_course': 'sqlite:///gd_course.db',
                                           'gender_platform': 'sqlite:///gender_platform.db'  }
 
 #application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dvqbylgvsfphjq:e42fe38ff4158166fc3fea2f03c9af6c391a5715dbc4ddc940c94368bd4eb1c0@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/dd5gshjjree6ts'
