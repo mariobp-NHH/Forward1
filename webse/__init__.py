4881fa90a5d19ff87c9d7ad46db208ec482887aa
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-11-25 08:55:42 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -9,7 +9,7 @@ application.config['SECRET_KEY'] = '1dfc4dedcd5b2ffa3a090dfc34f845fd'
 # application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 #application.config['SQLALCHEMY_BINDS'] ={'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db'  }
 
-DBVAR = "postgresql://wurkinhbpcfdwh:b8df31f507916224e2ee03ca102a43b17ca5c835e75f442df57f97c3aec4fa67@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/df6hfd1ok5nj3r"
+DBVAR = "postgresql://rqhybwqehprrdc:d69429fcd0aeaa01d4badf7ba5a7cf8ea5fadc3b33c1c41eaade50a3485596d8@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/d77hmedg6s7p3c"
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
 application.config['SQLALCHEMY_BINDS'] ={'gd_course': DBVAR, 'gender_platform': DBVAR }
 db = SQLAlchemy(application)
