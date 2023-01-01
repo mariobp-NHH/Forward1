37523ea5d9911f9b50909a04d522ad4c6c1a65c4
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-01-01 21:35:02 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -9,12 +9,12 @@ application = Flask(__name__)
 DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
 application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'se_course': DBVAR, 'gd_course': DBVAR , 'gender_platform': DBVAR}
+application.config['SQLALCHEMY_BINDS'] ={'se_course': DBVAR, 'gd_course': DBVAR , 'gender_platform': DBVAR, 'se_platform': DBVAR}
 
 # DBVAR = 'sqlite:///user.db'
 # application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
 # application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-# application.config['SQLALCHEMY_BINDS'] ={'se_course': 'sqlite:///se_course.db', 'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db'}
+# application.config['SQLALCHEMY_BINDS'] ={'se_course': 'sqlite:///se_course.db', 'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db', 'se_platform': 'sqlite:///se_platform.db'}
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
@@ -38,6 +38,12 @@ from webse.gender_platform_chats.routes import gender_platform_chats
 from webse.gender_platform_questionnaires.routes import gender_platform_questionnaires
 from webse.gd_course_routes.routes import gd_course
 from webse.gd_course_chats.routes import gd_course_chats
+from webse.gd_course_announcements.routes import gd_course_announcements
+from webse.gd_course_app_calculator.routes import gd_course_app_calculator
+from webse.gd_course_questionnaires.routes import gd_course_questionnaires
+from webse.gd_course_students_apps.routes import gd_course_students_apps
+from webse.gd_course_app_calculator_HVL_2023_student1.routes import gd_course_app_calculator_HVL_2023_student1
+from webse.boilerplates.routes import boilerplates
 
 application.register_blueprint(forward_home)
 application.register_blueprint(forward_users)
@@ -54,3 +60,9 @@ application.register_blueprint(gender_platform_chats)
 application.register_blueprint(gender_platform_questionnaires)
 application.register_blueprint(gd_course)
 application.register_blueprint(gd_course_chats)
+application.register_blueprint(gd_course_announcements)
+application.register_blueprint(gd_course_app_calculator)
+application.register_blueprint(gd_course_questionnaires)
+application.register_blueprint(gd_course_students_apps)
+application.register_blueprint(gd_course_app_calculator_HVL_2023_student1)
+application.register_blueprint(boilerplates)
