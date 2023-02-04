5df208ad6b16045d437d071ab17e486d01aefd3e
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-04 10:54:26 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -6,22 +6,21 @@ import os
 
 application = Flask(__name__)
 
-DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'se_course': DBVAR, 'gd_course': DBVAR , 'gender_platform': DBVAR, 'se_platform': DBVAR}
-
-# DBVAR = 'sqlite:///user.db'
+# DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
 # application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-# application.config['SQLALCHEMY_BINDS'] ={'se_course': 'sqlite:///se_course.db', 'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db', 'se_platform': 'sqlite:///se_platform.db'}
+# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+# application.config['SQLALCHEMY_BINDS'] ={'se_course': DBVAR, 'gd_course': DBVAR , 'gender_platform': DBVAR, 'se_platform': DBVAR}
+
+DBVAR = 'sqlite:///user.db'
+application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
+application.config['SQLALCHEMY_BINDS'] ={'se_course': 'sqlite:///se_course.db', 'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db', 'se_platform': 'sqlite:///se_platform.db'}
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
 login_manager= LoginManager(application)
 login_manager.login_view = 'forward_users.forward_users_login'
 login_manager.login_message_category = 'info'
-
 
 from webse.forward_home.routes import forward_home
 from webse.forward_users.routes import forward_users
@@ -45,6 +44,7 @@ from webse.gd_course_students_apps.routes import gd_course_students_apps
 from webse.gd_course_chapters.routes import gd_course_chapters
 from webse.gd_course_statistics.routes import gd_course_statistics
 from webse.gd_course_app_calculator_HVL_2023_group1.routes import gd_course_app_calculator_HVL_2023_group1
+from webse.gd_course_app_calculator_2023_ch6.routes import gd_course_app_carbon_app_2023_ch6
 from webse.boilerplates.routes import boilerplates
 
 application.register_blueprint(forward_home)
@@ -69,4 +69,5 @@ application.register_blueprint(gd_course_students_apps)
 application.register_blueprint(gd_course_chapters)
 application.register_blueprint(gd_course_statistics)
 application.register_blueprint(gd_course_app_calculator_HVL_2023_group1)
+application.register_blueprint(gd_course_app_carbon_app_2023_ch6)
 application.register_blueprint(boilerplates)
