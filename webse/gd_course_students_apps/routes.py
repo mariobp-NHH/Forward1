7dbd659094aae17e7820b1e12a760315bde3db32
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-04-30 14:29:35 +0200

    Update routes.py

--- webse/gd_course_students_apps/routes.py
+++ webse/gd_course_students_apps/routes.py
@@ -7,7 +7,6 @@ from webse.forward_users.utils import read_image
 gd_course_students_apps= Blueprint('gd_course_students_apps', __name__)
 
 @gd_course_students_apps.route('/green_digitalization_course/gd_students_apps', methods=['GET', 'POST'])
-@login_required
 def gd_students_apps_home(): 
     return render_template('gd_course/students_apps/gd_students_apps_home.html', title='Students Apps Home')
 
