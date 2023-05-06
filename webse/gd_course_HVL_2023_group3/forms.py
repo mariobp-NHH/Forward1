0f18d98b5763f0179ae8b91d1ffc05de1bef719b
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-05-06 21:01:24 +0200

    Add files via upload

---
+++ webse/gd_course_HVL_2023_group3/forms.py
@@ -0,0 +1,69 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField,  SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired, NumberRange 
+from webse.models import User
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', 
+                         validators=[DataRequired(), Length(min=2, max=30)])
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  confirm_password = PasswordField('Confirm Password',
+                      validators=[DataRequired(), EqualTo('password')])                                
+  submit = SubmitField('Sign Up')
+
+  def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+  def validate_email(self, email):
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+          raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login') 
+  
+class BusForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Diesel', 'Diesel'), ('CNG', 'CNG'), ('No Fossil Fuel', 'No Fossil Fuel')])
+  submit = SubmitField('Submit')
+
+class CarForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  fuel_type = SelectField('Type of Fuel', [InputRequired()], 
+    choices=[('Petrol', 'Petrol'), ('Diesel', 'Diesel'), ('No Fossil Fuel', 'No Fossil Fuel')])
+  submit = SubmitField('Submit')  
+
+class PlaneForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  submit = SubmitField('Submit')
+  
+class FerryForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  brought_on = SelectField('How are you riding the ferry', [InputRequired()],
+    choices=[('On Foot', 'On Foot'), ('By Car', 'By Car')])
+  submit = SubmitField('Submit')  
+
+class MotorbikeForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  submit = SubmitField('Submit')
+
+class BikeWalkForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  submit = SubmitField('Submit')  
+
+class TramForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  submit = SubmitField('Submit')
+
+class TrainForm(FlaskForm):
+  kms = FloatField('Kilometers', [InputRequired()])
+  submit = SubmitField('Submit')  
