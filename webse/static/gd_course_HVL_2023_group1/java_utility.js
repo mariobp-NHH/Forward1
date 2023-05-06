5a6461a7548b22124cb0bbb3a5460946cc0d74f9
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-05-07 00:09:28 +0200

    Add files via upload

---
+++ webse/static/gd_course_HVL_2023_group1/java_utility.js
@@ -0,0 +1,199 @@
+function togglePersonInfo1() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-1");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-1");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+function togglePersonInfo2() {
+    // Hent knappen ved hjelp av ID-attributtet
+    var toggleButton = document.getElementById("toggle-button-2");
+    // Sett opp en klikk-hendelse for knappen
+    toggleButton.addEventListener("click", function() {
+
+      // Hent teksten som skal skjules/avsløres
+      var text = document.getElementById("person-info-2");
+
+      // Skjul/avslør teksten ved å endre CSS display-attributten
+      if (text.style.display === "none") {
+        text.style.display = "block";
+        toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+      } else {
+        text.style.display = "none";
+        toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+      }
+    });
+    }
+function togglePersonInfo3() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-3");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-3");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo4() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-4");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-4");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo5() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-5");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-5");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo6() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-6");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-6");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo7() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-7");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-7");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo8() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-8");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-8");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo9() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-9");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-9");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+
+function togglePersonInfo10() {
+  // Hent knappen ved hjelp av ID-attributtet
+  var toggleButton = document.getElementById("toggle-button-10");
+  // Sett opp en klikk-hendelse for knappen
+  toggleButton.addEventListener("click", function() {
+
+    // Hent teksten som skal skjules/avsløres
+    var text = document.getElementById("person-info-10");
+
+    // Skjul/avslør teksten ved å endre CSS display-attributten
+    if (text.style.display === "none") {
+      text.style.display = "block";
+      toggleButton.innerHTML = '<span id="toggle-text">Read less</span>  <i class="fas fa-caret-up"></i>';
+    } else {
+      text.style.display = "none";
+      toggleButton.innerHTML = '<span id="toggle-text">Read more</span>  <i class="fas fa-caret-down"></i>';
+    }
+  });
+  }
+  
+
