846054bed06a35df385ddcdad82d5ae772a1374a
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-07-30 13:56:09 +0200

    Update routes.py

--- webse/se_platform_home/routes.py
+++ webse/se_platform_home/routes.py
@@ -7,6 +7,6 @@ se_platform_home= Blueprint('se_platform_home', __name__)
 @se_platform_home.route('/sustainable_energy_platform')
 def se_platform_home_home():
     page = request.args.get('page', 1, type=int)
-    announcements = ChatSEP.query.order_by(ChatSEP.date_posted.desc()).paginate(page=page, per_page=1)
+    announcements = ChatSEP.query.filter_by(chat_module='home').order_by(ChatSEP.date_posted.desc()).paginate(page=page, per_page=1)
     return render_template('se_platform/se_platform_home.html', announcements=announcements, title='Home', func=read_image)
 
