34cf509d4d3998b1aa657cb924d9ba625466e4f7
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-09-12 08:52:27 +0200

    Update routes.py

--- webse/sbm_course_home/routes.py
+++ webse/sbm_course_home/routes.py
@@ -9,6 +9,6 @@ sbm_course= Blueprint('sbm_course', __name__)
 def sbm_course_home():
     page = request.args.get('page', 1, type=int)
     announcements = AnnouncementGD.query.order_by(AnnouncementGD.date_posted.desc()).\
-        filter((AnnouncementGD.user_id==1) | (AnnouncementGD.user_id==2)).paginate(page=page, per_page=1)
+        filter((AnnouncementGD.user_id==3) | (AnnouncementGD.user_id==2)).paginate(page=page, per_page=1)
     return render_template('sbm_course/sbm_course_home.html', announcements=announcements, title='Sustainable Business Models Course', func=read_image)
   
