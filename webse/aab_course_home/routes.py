ff70e3512a39e3f9e6102bfdc0df4deb3b647700
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-11-04 16:35:46 +0100

    Update routes.py

--- webse/aab_course_home/routes.py
+++ webse/aab_course_home/routes.py
@@ -8,6 +8,6 @@ aab_course= Blueprint('aab_course', __name__)
 def aab_course_home():
     page = request.args.get('page', 1, type=int)
     announcements = AnnouncementGD.query.order_by(AnnouncementGD.date_posted.desc()).\
-        filter((AnnouncementGD.user_id==172) | (AnnouncementGD.user_id==173)).paginate(page=page, per_page=1)
+        filter((AnnouncementGD.user_id==198)).paginate(page=page, per_page=1)
     return render_template('aab_course/aab_course_home.html', announcements=announcements, title='Auditing Accounting and Business Course', func=read_image)
  
