c15451c32c14307f9a3f3fb153eeac2765c69749
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-11-12 12:01:22 +0100

    Update forms.py

--- webse/aab_course_business_module/forms.py
+++ webse/aab_course_business_module/forms.py
@@ -45,4 +45,76 @@ class ModulsForm_m2_ch1_q2(FlaskForm):
                        choices=[('Heating in buildings', 'Heating in buildings'),
                                 ('Incineration, and gasification to obtain biodiesel and hydrogen', 'Incineration, and gasification to obtain biodiesel and hydrogen'),
                                 ('Compost to be used in agriculture', 'Compost to be used in agriculture')])
-    submit = SubmitField('Submit')   
+    submit = SubmitField('Submit') 
+
+
+# Chapter 2
+class ModulsForm_m2_ch2_e1(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Demand is difficult to forecast', 'Demand is difficult to forecast'),
+                                ('Storage is prohibitively costly', 'Storage is prohibitively costly'),
+                                ('Demand and supply have to match all the time', 'Demand and supply have to match all the time')])
+    submit = SubmitField('Submit')
+
+class ModulsForm_m2_ch2_e2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('The transmission line can be congested', 'The transmission line can be congested'),
+                                ('Low demand elasticity', 'Low demand elasticity'),
+                                ('Electricity cannot be stored', 'Electricity cannot be stored')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_e3(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('The introduction of sensors at home', 'The introduction of sensors at home'),
+                                ('The introduction of sensors in the grid', 'The introduction of sensors in the grid'),
+                                ('Real time electricity pricing apps', 'Real time electricity pricing apps')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_e4(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Yes, substantially', 'Yes, substantially'),
+                                ('Yes, slightly', 'Yes, slightly'),
+                                ('No', 'No')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_q1(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Storage is prohibitively costly', 'Storage is prohibitively costly'),
+                                ('Electricity is produced in remote rural areas', 'Electricity is produced in remote rural areas'),
+                                ('Demand and supply have to match all the time', 'Demand and supply have to match all the time')])
+    submit = SubmitField('Submit')
+
+class ModulsForm_m2_ch2_q2(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Long-term contracts ensure electricity consumption in the long term', 'Long-term contracts ensure electricity consumption in the long term'),
+                                ('Long-term contracts guarantee a deterministic price', 'Long-term contracts guarantee a deterministic price'),
+                                ('Long-term contracts reduce the incentives to restric output', 'Long-term contracts reduce the incentives to restric output')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_q3(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Electricity consumption is stable during the day', 'Electricity consumption is stable during the day'),
+                                ('The non-storability of electricity', 'The non-storability of electricity'),
+                                ('The very low elasticity of demand for electricity', 'The very low elasticity of demand for electricity')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_q4(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('An active demand side that can respond to spot market price signals', 'An active demand side that can respond to spot market price signals'),
+                                ('Stable consumption in the industry', 'Stable consumption in the industry'),
+                                ('Forward contracts to mitigate market power', 'Forward contracts to mitigate market power')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_q5(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Creates value to the customers that are willing to make upwards or downwards adjustments in their consumption', 'Creates value to the customers that are willing to make upwards or downwards adjustments in their consumption'),
+                                ('Foster the development of renewable energy', 'Foster the development of renewable energy'),
+                                ('Reduce the consumption of energy', 'Reduce the consumption of energy')])
+    submit = SubmitField('Submit') 
+
+class ModulsForm_m2_ch2_q6(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('Solve the congestion problems in the grid that connect different countries', 'Solve the congestion problems in the grid that connect different countries'),
+                                ('Solve the congestion problems in the grid at local level', 'Solve the congestion problems in the grid at local level'),
+                                ('Solve supply fluctuation of electricity du to the introduction of renewable energy', 'Solve supply fluctuation of electricity du to the introduction of renewable energy')])
+    submit = SubmitField('Submit') 
