8d9659cd9d65741a21e630729ecd4681f7569b60
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-02-11 10:33:04 +0100

    Update routes.py

--- webse/gd_course_app_calculator/routes.py
+++ webse/gd_course_app_calculator/routes.py
@@ -239,3 +239,54 @@ def fuel_type(transport):
         fuelArray.append(fuelObj)
 
     return jsonify({"fuel_json": fuelArray})
+
+# REST API
+
+stores = [
+    {"name": "My Store", 
+     "items": 
+        [
+            {
+                "name": "Chair", 
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@gd_course_app_calculator.get("/green_digitalization_course/app_calculator/store")
+def get_stores():
+    return {"stores": stores}
+
+@gd_course_app_calculator.post("/green_digitalization_course/app_calculator/store")
+def create_store():   
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@gd_course_app_calculator.post("/green_digitalization_course/app_calculator/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@gd_course_app_calculator.get("/green_digitalization_course/app_calculator/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@gd_course_app_calculator.get("/green_digitalization_course/app_calculator/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
