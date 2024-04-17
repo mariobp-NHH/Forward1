951d2aab7c0fd21e8bafa89249e075a46b2b62e1
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-04-17 23:16:48 +0200

    Update routes.py

--- webse/gd_course_students_apps/routes.py
+++ webse/gd_course_students_apps/routes.py
@@ -6,7 +6,17 @@ from webse.forward_users.utils import read_image
 
 gd_course_students_apps= Blueprint('gd_course_students_apps', __name__)
 
-@gd_course_students_apps.route('/green_digitalization_course/gd_students_apps', methods=['GET', 'POST'])
+@gd_course_students_apps.route('/green_digitalization_course/gd_students_apps')
 def gd_students_apps_home(): 
     return render_template('gd_course/students_apps/gd_students_apps_home.html', title='Students Apps Home')
+
+@gd_course_students_apps.route('/green_digitalization_course/gd_students_apps/2024')
+def gd_students_apps_home_2024(): 
+    return render_template('gd_course/students_apps/gd_students_apps_home_2024.html', title='Students Apps Home 2024')
+
+@gd_course_students_apps.route('/green_digitalization_course/gd_students_apps/2023')
+def gd_students_apps_home_2023(): 
+    return render_template('gd_course/students_apps/gd_students_apps_home_2023.html', title='Students Apps Home 2023')
+
+
 
