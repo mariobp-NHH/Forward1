5655b904ef94e79b28ae4e56f5bc64659cc208db
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-04-19 19:28:04 +0200

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -51,6 +51,7 @@ login_manager.blueprint_login_views = {
     'gd_course_NHH_2024_group1' : 'gd_course_NHH_2024_group1.login',
     'gd_course_NHH_2024_group2' : 'gd_course_NHH_2024_group2.login',
     'gd_course_NHH_2024_group3' : 'gd_course_NHH_2024_group3.login',
+    'gd_course_NHH_2024_group4' : 'gd_course_NHH_2024_group4.login',
     'sbm_course_chats': 'forward_users.forward_users_login', 
     'aab_course_business_module': 'forward_users.forward_users_login', 
     'aab_course_announcements': 'forward_users.forward_users_login', 
@@ -100,6 +101,7 @@ from webse.gd_course_NHH_2023_group3.routes import gd_course_NHH_2023_group3
 from webse.gd_course_NHH_2024_group1.routes import gd_course_NHH_2024_group1
 from webse.gd_course_NHH_2024_group2.routes import gd_course_NHH_2024_group2
 from webse.gd_course_NHH_2024_group3.routes import gd_course_NHH_2024_group3
+from webse.gd_course_NHH_2024_group4.routes import gd_course_NHH_2024_group4
 from webse.se_platform_home.routes import se_platform_home
 from webse.se_platform_announcements.routes import se_platform_announcements
 from webse.dashboards.routes import dashboards
@@ -164,6 +166,7 @@ application.register_blueprint(gd_course_NHH_2023_group3)
 application.register_blueprint(gd_course_NHH_2024_group1)
 application.register_blueprint(gd_course_NHH_2024_group2)
 application.register_blueprint(gd_course_NHH_2024_group3)
+application.register_blueprint(gd_course_NHH_2024_group4)
 application.register_blueprint(dashboards)
 application.register_blueprint(papers_spot_go)
 application.register_blueprint(se_platform_home)
