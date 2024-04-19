8e146218137c2fac6d33f7f49097d9c1bb692c1a
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-04-19 09:58:10 +0200

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -44,6 +44,7 @@ login_manager.blueprint_login_views = {
     'gd_course_HVL_2024_group2': 'gd_course_HVL_2024_group2.login',
     'gd_course_HVL_2024_group3': 'gd_course_HVL_2024_group3.login',
     'gd_course_HVL_2024_group4': 'gd_course_HVL_2024_group4.login',
+    'gd_course_HVL_2024_group5': 'gd_course_HVL_2024_group5.login',
     'gd_course_NHH_2023_group1': 'gd_course_NHH_2023_group1.login',
     'gd_course_NHH_2023_group2': 'gd_course_NHH_2023_group2.login',
     'gd_course_NHH_2023_group3' : 'gd_course_NHH_2023_group3.login',
@@ -92,6 +93,7 @@ from webse.gd_course_HVL_2024_group1.routes import gd_course_HVL_2024_group1
 from webse.gd_course_HVL_2024_group2.routes import gd_course_HVL_2024_group2
 from webse.gd_course_HVL_2024_group3.routes import gd_course_HVL_2024_group3
 from webse.gd_course_HVL_2024_group4.routes import gd_course_HVL_2024_group4
+from webse.gd_course_HVL_2024_group5.routes import gd_course_HVL_2024_group5
 from webse.gd_course_NHH_2023_group1.routes import gd_course_NHH_2023_group1
 from webse.gd_course_NHH_2023_group2.routes import gd_course_NHH_2023_group2
 from webse.gd_course_NHH_2023_group3.routes import gd_course_NHH_2023_group3
@@ -155,6 +157,7 @@ application.register_blueprint(gd_course_HVL_2024_group1)
 application.register_blueprint(gd_course_HVL_2024_group2)
 application.register_blueprint(gd_course_HVL_2024_group3)
 application.register_blueprint(gd_course_HVL_2024_group4)
+application.register_blueprint(gd_course_HVL_2024_group5)
 application.register_blueprint(gd_course_NHH_2023_group1)
 application.register_blueprint(gd_course_NHH_2023_group2)
 application.register_blueprint(gd_course_NHH_2023_group3)
