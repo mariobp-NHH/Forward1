2495a4ad7dbdaafb83ff5c26b6b00949d7174d94
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-04-20 12:10:13 +0200

    Update routes.py

--- webse/gd_course_HVL_2024_group4/routes.py
+++ webse/gd_course_HVL_2024_group4/routes.py
@@ -66,23 +66,22 @@ def new_entry():
 
 
 # Emissions factor per transport in kg per passenger km
-efco2 = {
-    'Bicycle': {'Non-electric': 0, 'Electric': 0.006},
-    'Scooter': {'Electric': 0.015, 'Gasoline': 0.075},
-    'Motorbike': {'Petrol': 0.129},
-    'Bus': {'Diesel': 0.025, 'Biodiesel': 0.007},
-    'Train': {'Diesel': 0.091, 'Electric (Nordic)': 0.007, 'Electric (Europe)': 0.024},
-    'Car': {
-        'Petrol': 0.167, 'Diesel': 0.137, 'Biodiesel': 0.037,
-        'Electric (Nordic)': 0.014, 'Electric (Europe)': 0.045
-    },
-    'Ferry': {'Diesel': 0.226},
-    'Plane': {
-        'Scheduled flight (economy)': 0.133,
-        'Charter flight (economy)': 0.118,
-        'Scheduled flight (business)': 0.298
-    }
-}
+efco2={'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
+    'Car':{'Petrol':0.18592,'Diesel':0.16453,'No Fossil Fuel':0},
+    'Plane':{'Petrol':0.24298},
+    'Ferry':{'Diesel':0.11131, 'CNG':0.1131, 'No Fossil Fuel':0},
+    'Motorbike':{'Petrol':0.09816,'No Fossil Fuel':0},
+    'Scooter':{'No Fossil Fuel':0},
+    'Bicycle':{'No Fossil Fuel':0},
+    'Walk':{'No Fossil Fuel':0}}
+efch4={'Bus':{'Diesel':2e-5,'CNG':2.5e-3,'Petrol':2e-5,'No Fossil Fuel':0},
+    'Car':{'Petrol':3.1e-4,'Diesel':3e-6,'No Fossil Fuel':0},
+    'Plane':{'Petrol':1.1e-4},
+    'Ferry':{'Diesel':3e-5, 'CNG':3e-5,'No Fossil Fuel':0},
+    'Motorbike':{'Petrol':2.1e-3,'No Fossil Fuel':0},
+    'Scooter':{'No Fossil Fuel':0},
+    'Bicycle':{'No Fossil Fuel':0},
+    'Walk':{'No Fossil Fuel':0}}
 
 #New entry bus
 @gd_course_HVL_2024_group4.route('/green_digitalization_course/HVL/2024/group4/carbon_app/new_entry_bus', methods=['GET','POST'])
