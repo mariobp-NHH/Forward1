dfc308c66b8abe91d3681b32038da9850cb8de9f
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-08-09 16:23:54 +0200

    Update routes.py

--- webse/es_course_routes/routes.py
+++ webse/es_course_routes/routes.py
@@ -8,6 +8,3 @@ def es_course_home():
     page = request.args.get('page', 1, type=int)
     announcements = AnnouncementES.query.order_by(AnnouncementES.date_posted.desc()).filter(AnnouncementES.user_id==1).paginate(page=page, per_page=1)
     return render_template('es_course/es_course_home.html', announcements=announcements, title='Economies of Spanish Course', func=read_image)
-  
-
-  
